907c5d9c1beaddc17b527cfeab8476e180fd7d1b
renan <renanfgguimaraes7@gmail.com>
2022-06-30 21:38:15 -0300

    inicial

---
+++ telas.py
@@ -0,0 +1,53 @@
+from kivy.uix.screenmanager import Screen
+
+
+class HomePage(Screen):
+    pass
+
+
+class AjustesPage(Screen):
+    pass
+
+
+class AdicionarDocPage(Screen):
+    pass
+
+
+class ListarVendedoresPage(Screen):
+    pass
+
+
+class FotoPerfilPage(Screen):
+    pass
+
+
+class AdicionarVendedorPage(Screen):
+    pass
+
+
+class TodasVendasPage(Screen):
+    pass
+
+
+class LoginPage(Screen):
+    pass
+
+
+class VendasOutroVendedorPage(Screen):
+    pass
+
+
+class PaginaRgcpfPage(Screen):
+    pass
+
+
+class PaginaCarteirinhaePage(Screen):
+    pass
+
+
+class PaginaInfoMedicasPage(Screen):
+    pass
+
+
+class PaginaAvisosImportantesPage(Screen):
+    pass
